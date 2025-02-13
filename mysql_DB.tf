901806789dd2630648343050fc135571c975c91c
DenCap <dionisiecaprosu@Dionisies-MacBook-Pro.local>
2025-02-12 22:55:35 -0600

    added variables for db user and db pasword

--- mysql_DB.tf
+++ mysql_DB.tf
@@ -11,8 +11,8 @@ resource "aws_db_instance" "mysql" {
   engine                 = "mysql"
   # engine_version         = "8.0"
   instance_class         = "db.t3.micro"
-  username               = "admin"
-  password               = "adminadmin"
+  username               = var.db_username
+  password               = var.db_username_password
   db_subnet_group_name   = aws_db_subnet_group.mysql_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
 
--- variables.tf
+++ variables.tf
@@ -1,2 +1,4 @@
 variable "ami_id" {}
 variable "ssh-key" {}
+variable "db_username" {}
+variable "db_username_password" {}
